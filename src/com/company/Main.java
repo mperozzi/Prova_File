c11c42d892ac66b855ae0a02fa05e2e0dd987db2
Matteo Perozzi <mat.perozzi@github.com>
2019-06-15 21:01:55 +0200

    Rev 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+        Show_File sf = new Show_File();
+        sf.WriteFile("C:\\Users\\Matteo\\Desktop\\prova.txt");
     }
 }
--- src/com/company/Show_File.java
+++ src/com/company/Show_File.java
@@ -1,7 +1,6 @@
 package com.company;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -14,21 +13,24 @@ public class Show_File {
         PrintWriter pw = new PrintWriter(System.out, true);
 
         if (namefile.length() < 1) {
-            pw.println("Specificare un nome di un file");
+            System.out.print("Specificare un nome di un file");
             return;
         }
 
         //apro il file e leggo di seguito fino a che non trovo eof (-1)
         try {
+            //istanza di un flusso per leggere il file
             fis = new FileInputStream(namefile);
             do {
+                //leggo tramute il flusso i caratteri nel file
                 i = fis.read();
+                //se non sono alla fine del file (eof = -1) stampo a schermo il carattere letto (che in realtà è intero e viene convertito)
                 if (i != -1) {
-                    pw.println((char) i);
+                    System.out.print((char) i);
                 }
             }   while (i != -1);
 
-            //gestione delle eccezioni --> Generale
+            //gestione delle eccezioni --> Generale --> Non mi interessa che tipo di errore è
         } catch (java.io.IOException e) {
             pw.println("Errore: " + e);
 
